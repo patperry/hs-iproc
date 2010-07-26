9af5bf705a433f2798aa5bc164627cd226ed3d48
Patrick Perry <patperry@gmail.com>
2010-07-26 14:52:55 -0400

    Factor out ActorSet from Actor module

--- Actor.hs
+++ Actor.hs
@@ -7,20 +7,7 @@ module Actor (
     ReceiverId,
     SenderId,
     
-    ActorSet,
-    SenderSet,
-    ReceiverSet,
-    
-    actorSet,
-    actorIndex,
-    actorAt,
-    actorCount,
-    actorAssocList,
-    actorList,
   ) where
-
-import Data.IntMap( IntMap )
-import qualified Data.IntMap as IntMap
 
 import Numeric.LinearAlgebra( Vector )
 
@@ -32,33 +19,7 @@ data Actor =
     Actor { actorId :: !ActorId
           , actorVars :: !(Vector Double)
           }
-    deriving (Show)
+    deriving (Eq, Show)
               
 type Sender = Actor
 type Receiver = Actor      
-
-data ActorSet = ActorSet !Int !(IntMap Actor) !(IntMap Int)
-
-type SenderSet = ActorSet
-type ReceiverSet = ActorSet
-
-
-actorSet :: [Actor] -> ActorSet
-actorSet as = ActorSet (length as)
-                       (IntMap.fromAscList $ zip [ 0.. ] as)
-                       (IntMap.fromList $ zip (map actorId as) [ 0.. ])
-
-actorIndex :: Actor -> ActorSet -> Maybe Int
-actorIndex a (ActorSet _ _ idx) = IntMap.lookup (actorId a) idx
-
-actorAt :: Int -> ActorSet -> Maybe Actor
-actorAt i (ActorSet _ as _) = IntMap.lookup i as
-
-actorCount :: ActorSet -> Int
-actorCount (ActorSet n _ _) = n
-
-actorAssocList :: ActorSet -> [(Int,Actor)]
-actorAssocList (ActorSet _ as _) = IntMap.assocs as
-
-actorList :: ActorSet -> [Actor]
-actorList (ActorSet _ as _) = IntMap.elems as
---
+++ ActorSet.hs
@@ -0,0 +1,44 @@
+module ActorSet (
+    ActorSet,
+    SenderSet,
+    ReceiverSet,
+    
+    fromList,
+    lookup,
+    at,
+    size,
+    assocs,
+    toList,
+    ) where
+    
+import Prelude hiding ( lookup )
+import Data.IntMap( IntMap )
+import qualified Data.IntMap as IntMap
+
+import Actor
+
+data ActorSet = ActorSet !Int !(IntMap Actor) !(IntMap Int) deriving (Eq,Show)
+
+type SenderSet = ActorSet
+type ReceiverSet = ActorSet
+
+
+fromList :: [Actor] -> ActorSet
+fromList as = ActorSet (length as)
+                       (IntMap.fromAscList $ zip [ 0.. ] as)
+                       (IntMap.fromList $ zip (map actorId as) [ 0.. ])
+
+lookup :: Actor -> ActorSet -> Maybe Int
+lookup a (ActorSet _ _ idx) = IntMap.lookup (actorId a) idx
+
+at :: Int -> ActorSet -> Maybe Actor
+at i (ActorSet _ as _) = IntMap.lookup i as
+
+size :: ActorSet -> Int
+size (ActorSet n _ _) = n
+
+assocs :: ActorSet -> [(Int,Actor)]
+assocs (ActorSet _ as _) = IntMap.assocs as
+
+toList :: ActorSet -> [Actor]
+toList (ActorSet _ as _) = IntMap.elems as
