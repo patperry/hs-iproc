d29bce9994ea643e464450f7aaec0e004a3af156
Patrick Perry <patperry@gmail.com>
2010-07-26 18:38:41 -0400

    Add IntervalSet module and test Properties

---
+++ IntervalSet.hs
@@ -0,0 +1,56 @@
+module IntervalSet (
+    IntervalSet,
+    IntervalId,
+    
+    size,
+    at,
+    lookup,
+    assocs,
+    
+    fromList,
+    toList,
+    ) where
+        
+import Prelude hiding ( lookup )
+
+import Data.List( findIndex, nub, sort )
+import Data.Map( Map )
+import qualified Data.Map as Map
+import Data.Maybe( fromJust )
+import Data.Time
+
+type IntervalId = Int
+data IntervalSet = IntervalSet !Int !(Map IntervalId DiffTime) deriving (Eq)
+
+fromList :: [DiffTime] -> IntervalSet
+fromList ts | sort ts /= ts = error "time interval list is not sorted"
+            | nub ts /= ts = error "time interval list is not unique"
+            | any (<= 0) ts = error "non-positive time interval"
+            | otherwise =
+        IntervalSet (length ts) $ Map.fromList $ zip [ 0.. ] ts
+
+toList :: IntervalSet -> [DiffTime]
+toList (IntervalSet _ m) = Map.elems m
+
+instance Show IntervalSet where
+    show iset = "fromList " ++ show (toList iset)
+
+
+size :: IntervalSet -> Int
+size (IntervalSet n m) = n
+
+at :: IntervalId -> IntervalSet -> DiffTime
+at i (IntervalSet n m) | i < 0 = error "negative index"
+                       | i >= n = error "index too large"
+                       | otherwise =
+    fromJust $ Map.lookup i m
+
+lookup ::  DiffTime -> IntervalSet -> Maybe IntervalId
+lookup t (IntervalSet _ m) | t <= 0 = Nothing
+                           | otherwise = do
+    i <- findIndex (>= t) $ 0:(Map.elems m)
+    return $ pred i
+    
+assocs :: IntervalSet -> [(IntervalId,DiffTime)]
+assocs (IntervalSet _ m) = Map.assocs m
+
--- Properties.hs
+++ Properties.hs
@@ -1,8 +1,9 @@
 module Properties
     where
 
+import Data.Time
 import Data.Function( on )
-import Data.List( sort, sortBy )
+import Data.List( nub, sort, sortBy )
 import Data.Maybe( fromJust )
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
@@ -12,8 +13,13 @@ import qualified Test.QuickCheck.LinearAlgebra as Test
 import Numeric.LinearAlgebra
 
 import Actor( Actor(..) )
+
 import ActorSet( ActorSet )
 import qualified ActorSet as ActorSet
+
+import IntervalSet (IntervalSet, IntervalId )
+import qualified IntervalSet as IntervalSet
+
 import SVars( SVars )
 import qualified SVars as SVars
 
@@ -61,6 +67,36 @@ instance Arbitrary ActorList where
         let as' = (snd . unzip . sortBy (compare `on` fst)) $ zip is as
         return $ ActorList as'
 
+
+instance Arbitrary DiffTime where
+    arbitrary = do
+        x <- arbitrary :: Gen Integer
+        return $ fromIntegral x
+
+data IntervalList = IntervalList [DiffTime] deriving (Show)
+instance Arbitrary IntervalList where
+    arbitrary = do
+        ts <- arbitrary
+        return $ IntervalList $ nub $ sort $ filter (> 0) ts
+
+instance Arbitrary IntervalSet where
+    arbitrary = do
+        (IntervalList ts) <- arbitrary
+        return $ IntervalSet.fromList ts
+
+newtype EmptyIntervalSet = EmptyIntervalSet IntervalSet deriving (Eq,Show)
+instance Arbitrary EmptyIntervalSet where
+    arbitrary = return $ EmptyIntervalSet $ IntervalSet.fromList []
+
+newtype NonEmptyIntervalSet = NonEmptyIntervalSet IntervalSet deriving (Eq,Show)
+instance Arbitrary NonEmptyIntervalSet where
+    arbitrary = do
+        t <- arbitrary
+        (IntervalList ts) <- arbitrary
+        return $ NonEmptyIntervalSet $
+            IntervalSet.fromList $ nub $ sort $ (abs t + 1):ts
+
+
 tests_ActorSet = testGroup "ActorSet"
     [ testProperty "size . fromList" prop_ActorSet_size_fromList
     , testProperty "lookup . fromList" prop_ActorSet_lookup_fromList
@@ -80,7 +116,7 @@ prop_ActorSet_lookup_fromList (ActorList as) = let
 
 prop_ActorSet_at_fromList (ActorList as) = let
     a_set = ActorSet.fromList as
-    in and [ ActorSet.at i a_set == Just a
+    in and [ ActorSet.at i a_set == a
            | (i,a) <- zip [ 0.. ] as 
            ]
            
@@ -134,7 +170,80 @@ prop_SVars_lookupSender_fromLists (ActorList ss) (ActorList rs) = let
                 == Just [ (actorId r, SVars.interactions s r) | r <- rs ] 
            | s <- ss ]
 
+
+tests_IntervalSet = testGroup "IntervalSet"
+    [ testProperty "size . fromList" prop_IntervalSet_size_fromList
+    , testProperty "at . fromList" prop_IntervalSet_at_fromList
+    , testProperty "assocs . fromList" prop_IntervalSet_assocs_fromList     
+    , testProperty "toList . fromList" prop_IntervalSet_toList_fromList 
+    , testProperty "fromList . toList" prop_IntervalSet_fromList_toList
+    , testProperty "lookup (empty)" prop_IntervalSet_lookup_empty
+    , testProperty "lookup (nonpositive)" prop_IntervalSet_lookup_nonpos
+    , testProperty "lookup (endpoint)" prop_IntervalSet_lookup_endpoint
+    , testProperty "lookup (before endpoint)" prop_IntervalSet_lookup_before_endpoint    
+    , testProperty "lookup (after endpoint)" prop_IntervalSet_lookup_after_endpoint        
+    , testProperty "lookkup (beyond last)" prop_IntervalSet_lookup_beyond_last
+    ]
+
+prop_IntervalSet_size_fromList (IntervalList ts) =
+    (IntervalSet.size . IntervalSet.fromList) ts == length ts
+    
+prop_IntervalSet_at_fromList (IntervalList ts) = let
+    iset = IntervalSet.fromList ts
+    in and [ IntervalSet.at i iset == t | (i,t) <- zip [ 0.. ] ts ]
+
+prop_IntervalSet_assocs_fromList (IntervalList ts) =
+    (IntervalSet.assocs . IntervalSet.fromList) ts == zip [ 0.. ] ts
+
+prop_IntervalSet_toList_fromList (IntervalList ts) =
+    (IntervalSet.toList . IntervalSet.fromList) ts == ts
+    
+prop_IntervalSet_fromList_toList iset =
+    (IntervalSet.fromList . IntervalSet.toList) iset == iset
+    
+prop_IntervalSet_lookup_empty t (EmptyIntervalSet iset) =
+    IntervalSet.lookup t iset == Nothing
+
+prop_IntervalSet_lookup_nonpos t iset =
+    IntervalSet.lookup (negate $ abs t) iset == Nothing
+    
+prop_IntervalSet_lookup_endpoint (NonEmptyIntervalSet iset) =
+    forAll (choose (0,n-1)) $ \i -> let
+        t = IntervalSet.at i iset
+        in IntervalSet.lookup t iset == Just i
+  where
+    n = IntervalSet.size iset
+
+prop_IntervalSet_lookup_before_endpoint (NonEmptyIntervalSet iset) =
+    forAll (choose (0,n-1)) $ \i -> let
+        t_begin = if i == 0 then 0 else IntervalSet.at (i-1) iset
+        t_end = IntervalSet.at i iset
+        t = t_end - picosecondsToDiffTime 1
+        in IntervalSet.lookup t iset ==
+            if t == 0 then Nothing
+                      else if t == t_begin then Just (i-1)
+                                           else Just i
+  where
+    n = IntervalSet.size iset
+
+prop_IntervalSet_lookup_after_endpoint (NonEmptyIntervalSet iset) =
+    forAll (choose (0,n-1)) $ \i -> let
+        t_begin = if i == 0 then 0 else IntervalSet.at (i-1) iset
+        t_end = IntervalSet.at i iset
+        t = t_begin + picosecondsToDiffTime 1
+        in IntervalSet.lookup t iset == Just i
+  where
+    n = IntervalSet.size iset
+
+prop_IntervalSet_lookup_beyond_last (NonEmptyIntervalSet iset) =
+    IntervalSet.lookup (tlast + picosecondsToDiffTime 1) iset == Nothing
+  where
+    n = IntervalSet.size iset
+    tlast = IntervalSet.at (n - 1) iset
+
+    
 main :: IO ()
 main = defaultMain [ tests_ActorSet
                    , tests_SVars
+                   , tests_IntervalSet
                    ]
