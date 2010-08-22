c0c5dab38f2f1656eade65dc3d5b8f461cf9cbd5
Patrick Perry <patperry@gmail.com>
2010-08-22 13:13:27 -0400

    Add option to use Int instead of NominalDiffTime

--- Types.hs
+++ Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, CPP #-}
 module Types (
     Actor(..),
     ActorId,
@@ -23,9 +23,28 @@ module Types (
 
 import Numeric.LinearAlgebra( Vector )
 
+
+#define FAST_TIME
+
+#ifndef FAST_TIME
+
 import Data.Time( NominalDiffTime, UTCTime, diffUTCTime, addUTCTime )
-import Data.Time.Clock()
 import Data.Time.Clock.POSIX( posixSecondsToUTCTime )
+
+#else
+    
+type UTCTime = Int
+type NominalDiffTime = Int
+
+diffUTCTime, addUTCTime :: Int -> Int -> Int
+diffUTCTime = (-)
+addUTCTime = (+)
+
+posixSecondsToUTCTime :: Int -> Int
+posixSecondsToUTCTime = id
+
+#endif
+
 
 newtype Time = Time { unTime :: UTCTime }
     deriving (Eq, Show, Ord)
@@ -40,6 +59,7 @@ diffTime t1 t2 = DiffTime $ diffUTCTime (unTime t1) (unTime t2)
 
 addTime :: DiffTime -> Time -> Time
 addTime dt t = Time $ addUTCTime (unDiffTime dt) (unTime t)
+
 
 
 type ActorId = Int
