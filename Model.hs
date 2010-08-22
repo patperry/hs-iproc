be8dc6023f92becc2ecf2e2328b08b9a3853f918
Patrick Perry <patperry@gmail.com>
2010-08-22 04:03:05 -0400

    Cache static probabilities

--- Model.hs
+++ Model.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
 module Model (
     Model,
     Loops(..),
@@ -28,133 +27,100 @@ module Model (
     logProbs,
     meanVars,
     covVars,
+    meanCovVars,
     ) where
         
-import Data.List( foldl' )
+import ModelBase( Loops(..) )
+import qualified ModelBase as Base
+
+import Data.Map( Map )
 import qualified Data.Map as Map
 import Numeric.LinearAlgebra
 
 import Actor( SenderId, ReceiverId )
 import History( History )
 import Vars( Vars )
+import qualified History as History
 import qualified Vars as Vars
 
-data Loops = Loops | NoLoops deriving (Eq, Show)
-
 data Model =
-    Model { vars :: !Vars
-          , coefs :: !(Vector Double)
-          , hasLoops :: !Bool
+    Model { base :: !(Base.Model)
+          , staticProbs :: !(Map (SenderId, ReceiverId) Double)
           }
     deriving (Show)
 
 fromVars :: Vars -> Vector Double -> Loops -> Model
-fromVars v c l
-    | Vars.dim v /= dimVector c =
-        error "fromVars: dimension mismatch"
-    | otherwise =
-        Model v c $ case l of { Loops -> True ; NoLoops -> False }
+fromVars v c l = let
+    m = Base.fromVars v c l
+    h0 = History.empty
+    sps = Map.fromList [ ((s,r),p) | s <- Base.senders m
+                                   , (r,p) <- Base.probs m h0 s ]
+    in Model m sps
+
+vars :: Model -> Vars
+vars = Base.vars . base
+
+coefs :: Model -> Vector Double
+coefs = Base.coefs . base
+
+hasLoops :: Model -> Bool
+hasLoops = Base.hasLoops . base
 
 validDyad :: Model -> SenderId -> ReceiverId -> Bool
-validDyad m s r | hasLoops m = True
-                | otherwise  = s /= r
+validDyad = Base.validDyad . base
 
 senders :: Model -> [SenderId]
-senders m = Vars.senders $ vars m
+senders = Base.senders . base
 
 receivers :: Model -> [ReceiverId]
-receivers m = Vars.receivers $ vars m
+receivers = Base.receivers . base
 
-validReceivers ::  Model ->  SenderId ->[ReceiverId]
-validReceivers m s = filter (validDyad m s) $ receivers m
+validReceivers ::  Model ->  SenderId -> [ReceiverId]
+validReceivers = Base.validReceivers . base
 
 weight :: Model -> History -> SenderId -> ReceiverId -> Double
-weight m h s r = exp $ logWeight m h s r
+weight = Base.weight . base
 
 logWeight :: Model -> History -> SenderId -> ReceiverId -> Double
-logWeight m h s r | not (validDyad m s r) = neginf
-                  | otherwise =
-    Vars.mulDyadBy (coefs m) (vars m) h s r
-  where
-    neginf = -1.0/(0.0 :: Double)
+logWeight = Base.logWeight . base
 
 logWeightChange :: Model -> History -> SenderId -> ReceiverId -> Double
-logWeightChange m h s r | not (validDyad m s r) = 0
-                        | otherwise =
-    Vars.mulDyadChangesBy (coefs m) (vars m) h s r
+logWeightChange = Base.logWeightChange . base
 
 sumWeights :: Model -> History -> SenderId -> Double
-sumWeights m h s = exp (logSumWeights m h s)
+sumWeights = Base.sumWeights . base
 
 logSumWeights :: Model -> History -> SenderId -> Double
-logSumWeights m h s = let
-    (lw_max, nlp_max) = logSumWeightsParts m h s
-    in lw_max + nlp_max
-
-logSumWeightsParts :: Model -> History -> SenderId -> (Double, Double)
-logSumWeightsParts m h s = let
-    lws = (snd . unzip) $ logWeights m h s
-    ilws = zip [ 0..] lws
-    (i_max,lw_max) = foldl' maxPair (0,neginf) ilws
-    nlp_max = log1p (foldl' (+) 0 [ exp (lw - lw_max)
-                                  | (i,lw) <- ilws, i /= i_max ])
-    in (lw_max, nlp_max)
-  where
-    maxPair (i1,e1) (i2,e2) =
-        let (i,e) = if e2 > e1 then (i2,e2) else (i1,e1)
-        in i `seq` e `seq` (i,e)
-    neginf = -1/0 :: Double
-
-foreign import ccall unsafe
-    log1p :: Double -> Double
+logSumWeights = Base.logSumWeights . base
 
 weights :: Model -> History -> SenderId -> [(ReceiverId, Double)]
-weights m h s = [ (r, exp lw) | (r,lw) <- logWeights m h s ]
+weights = Base.weights . base
 
 logWeights :: Model -> History -> SenderId -> [(ReceiverId, Double)]
-logWeights m h s = 
-    filter (validDyad m s . fst) $
-        Vars.mulSenderBy (coefs m) (vars m) h s
+logWeights = Base.logWeights . base
 
 logWeightChanges :: Model -> History -> SenderId -> [(ReceiverId, Double)]
-logWeightChanges m h s = 
-    filter (validDyad m s . fst) $ 
-        Vars.mulSenderChangesBy (coefs m) (vars m) h s
+logWeightChanges = Base.logWeightChanges . base
 
 prob :: Model -> History -> SenderId -> ReceiverId -> Double
-prob m h s r = min 1 $ exp (logProb m h s r)
+prob (Model m sps) h s r
+    | History.null h = Map.findWithDefault 0 (s,r) sps
+    | otherwise      = Base.prob m h s r
 
 logProb :: Model -> History -> SenderId -> ReceiverId -> Double
-logProb m h s r = let
-    lw = logWeight m h s r
-    (lw_max, nlp_max) = logSumWeightsParts m h s
-    in if lw == lw_max
-            then -nlp_max
-            else (lw - lw_max) - nlp_max
+logProb = Base.logProb . base
 
 probs :: Model -> History -> SenderId -> [(ReceiverId, Double)]
-probs m h s = [ (r, min 1 $ exp lp) | (r,lp) <- logProbs m h s ]
+probs = Base.probs . base
 
 logProbs :: Model -> History -> SenderId -> [(ReceiverId, Double)]
-logProbs m h s = let
-    rlws = logWeights m h s
-    (lw_max, nlp_max) = logSumWeightsParts m h s
-    in [ (r, if lw == lw_max
-                 then -nlp_max
-                 else (lw - lw_max) - nlp_max)
-       | (r,lw) <- rlws
-       ]
+logProbs = Base.logProbs . base
 
 meanVars :: Model -> History -> SenderId -> Vector Double
-meanVars m h s = fst $ meanCovVars m h s
+meanVars = Base.meanVars . base
 
 covVars :: Model -> History -> SenderId -> Herm Matrix Double
-covVars m h s = snd $ meanCovVars m h s
+covVars = Base.covVars . base
 
 meanCovVars :: Model -> History -> SenderId -> (Vector Double, Herm Matrix Double)
-meanCovVars m h s = let
-    v = vars m
-    wxs = [ (w, Vars.dyad v h s r) | (r,w) <- probs m h s ]
-    mu = weightedMeanVector (Vars.dim v) wxs
-    sigma = weightedCovMatrixWithMean mu MLCov wxs
-    in (mu, sigma)
+meanCovVars = Base.meanCovVars . base
---
+++ ModelBase.hs
@@ -0,0 +1,161 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+module ModelBase (
+    Model,
+    Loops(..),
+    fromVars,
+
+    vars,
+    coefs,
+    hasLoops,
+    senders,
+    receivers,
+    validDyad,
+    validReceivers,
+    
+    weight,
+    logWeight,
+    logWeightChange,
+
+    weights,
+    logWeights,
+    logWeightChanges,
+    sumWeights,
+    logSumWeights,
+
+    prob,
+    logProb,
+    probs,
+    logProbs,
+    meanVars,
+    covVars,
+    meanCovVars,
+    ) where
+        
+import Data.List( foldl' )
+import qualified Data.Map as Map
+import Numeric.LinearAlgebra
+
+import Actor( SenderId, ReceiverId )
+import History( History )
+import Vars( Vars )
+import qualified Vars as Vars
+
+data Loops = Loops | NoLoops deriving (Eq, Show)
+
+data Model =
+    Model { vars :: !Vars
+          , coefs :: !(Vector Double)
+          , hasLoops :: !Bool
+          }
+    deriving (Show)
+
+fromVars :: Vars -> Vector Double -> Loops -> Model
+fromVars v c l
+    | Vars.dim v /= dimVector c =
+        error "fromVars: dimension mismatch"
+    | otherwise =
+        Model v c $ case l of { Loops -> True ; NoLoops -> False }
+
+validDyad :: Model -> SenderId -> ReceiverId -> Bool
+validDyad m s r | hasLoops m = True
+                | otherwise  = s /= r
+
+senders :: Model -> [SenderId]
+senders m = Vars.senders $ vars m
+
+receivers :: Model -> [ReceiverId]
+receivers m = Vars.receivers $ vars m
+
+validReceivers ::  Model ->  SenderId ->[ReceiverId]
+validReceivers m s = filter (validDyad m s) $ receivers m
+
+weight :: Model -> History -> SenderId -> ReceiverId -> Double
+weight m h s r = exp $ logWeight m h s r
+
+logWeight :: Model -> History -> SenderId -> ReceiverId -> Double
+logWeight m h s r | not (validDyad m s r) = neginf
+                  | otherwise =
+    Vars.mulDyadBy (coefs m) (vars m) h s r
+  where
+    neginf = -1.0/(0.0 :: Double)
+
+logWeightChange :: Model -> History -> SenderId -> ReceiverId -> Double
+logWeightChange m h s r | not (validDyad m s r) = 0
+                        | otherwise =
+    Vars.mulDyadChangesBy (coefs m) (vars m) h s r
+
+sumWeights :: Model -> History -> SenderId -> Double
+sumWeights m h s = exp (logSumWeights m h s)
+
+logSumWeights :: Model -> History -> SenderId -> Double
+logSumWeights m h s = let
+    (lw_max, nlp_max) = logSumWeightsParts m h s
+    in lw_max + nlp_max
+
+logSumWeightsParts :: Model -> History -> SenderId -> (Double, Double)
+logSumWeightsParts m h s = let
+    lws = (snd . unzip) $ logWeights m h s
+    ilws = zip [ 0..] lws
+    (i_max,lw_max) = foldl' maxPair (0,neginf) ilws
+    nlp_max = log1p (foldl' (+) 0 [ exp (lw - lw_max)
+                                  | (i,lw) <- ilws, i /= i_max ])
+    in (lw_max, nlp_max)
+  where
+    maxPair (i1,e1) (i2,e2) =
+        let (i,e) = if e2 > e1 then (i2,e2) else (i1,e1)
+        in i `seq` e `seq` (i,e)
+    neginf = -1/0 :: Double
+
+foreign import ccall unsafe
+    log1p :: Double -> Double
+
+weights :: Model -> History -> SenderId -> [(ReceiverId, Double)]
+weights m h s = [ (r, exp lw) | (r,lw) <- logWeights m h s ]
+
+logWeights :: Model -> History -> SenderId -> [(ReceiverId, Double)]
+logWeights m h s = 
+    filter (validDyad m s . fst) $
+        Vars.mulSenderBy (coefs m) (vars m) h s
+
+logWeightChanges :: Model -> History -> SenderId -> [(ReceiverId, Double)]
+logWeightChanges m h s = 
+    filter (validDyad m s . fst) $ 
+        Vars.mulSenderChangesBy (coefs m) (vars m) h s
+
+prob :: Model -> History -> SenderId -> ReceiverId -> Double
+prob m h s r = min 1 $ exp (logProb m h s r)
+
+logProb :: Model -> History -> SenderId -> ReceiverId -> Double
+logProb m h s r = let
+    lw = logWeight m h s r
+    (lw_max, nlp_max) = logSumWeightsParts m h s
+    in if lw == lw_max
+            then -nlp_max
+            else (lw - lw_max) - nlp_max
+
+probs :: Model -> History -> SenderId -> [(ReceiverId, Double)]
+probs m h s = [ (r, min 1 $ exp lp) | (r,lp) <- logProbs m h s ]
+
+logProbs :: Model -> History -> SenderId -> [(ReceiverId, Double)]
+logProbs m h s = let
+    rlws = logWeights m h s
+    (lw_max, nlp_max) = logSumWeightsParts m h s
+    in [ (r, if lw == lw_max
+                 then -nlp_max
+                 else (lw - lw_max) - nlp_max)
+       | (r,lw) <- rlws
+       ]
+
+meanVars :: Model -> History -> SenderId -> Vector Double
+meanVars m h s = fst $ meanCovVars m h s
+
+covVars :: Model -> History -> SenderId -> Herm Matrix Double
+covVars m h s = snd $ meanCovVars m h s
+
+meanCovVars :: Model -> History -> SenderId -> (Vector Double, Herm Matrix Double)
+meanCovVars m h s = let
+    v = vars m
+    wxs = [ (w, Vars.dyad v h s r) | (r,w) <- probs m h s ]
+    mu = weightedMeanVector (Vars.dim v) wxs
+    sigma = weightedCovMatrixWithMean mu MLCov wxs
+    in (mu, sigma)
